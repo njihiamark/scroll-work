65164c26649c8bd9202ebbfbd642f7770e426dc9
njihiamark <njihiamark@gmail.com>
2020-11-06 22:47:00 +0300

    initial setup of the infinite scroll

---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <React.Fragment>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default List;
