1cec0b1e028316d01b5cd1acbf69056def118bca
njihiamark <njihiamark@gmail.com>
2020-11-07 13:18:04 +0300

    instal semantic-ui

--- src/List.js
+++ src/List.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 
 const List = () => {
-  const [Items, seItems] = useState(Array.from(Array(20).keys(), (n) => n + 1));
+  const [Items, setItems] = useState(
+    Array.from(Array(20).keys(), (n) => n + 1)
+  );
+  const [isFetching, setIsFetching] = useState(false);
+
+  function loadMoreItems() {
+    setTimeout(() => {
+      setItems((prevState) => [
+        ...prevState,
+        ...Array.from(Array(20).keys(), (n) => n + prevState.length + 1),
+      ]);
+      setIsFetching(false);
+    }, 2000);
+  }
+
   return (
     <React.Fragment>
       <ul>
         {Items.map((item) => (
-          <li>Item {item}</li>
+          <li key={item}>Item {item}</li>
         ))}
       </ul>
+      {isFetching && "Fetching more items..."}
     </React.Fragment>
   );
 };
